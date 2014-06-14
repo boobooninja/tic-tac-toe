63f6ce5a5d19230e59a0b8f65fcf77cde0a61ca4
Loren Hale <loren.hale@gmail.com>
2014-06-14 11:24:54 -0500

    refactor logic and view code to separate files

---
+++ js/logic.js
@@ -0,0 +1,79 @@
+(function() {
+  var Game = function() {
+
+    this.spaces = [ NaN, NaN, NaN,
+                    NaN, NaN, NaN,
+                    NaN, NaN, NaN ];
+
+    this.player1 = 'veggies';
+    this.player1Score = 0;
+    this.player2 = 'junkfood';
+    this.player2Score = 0;
+    this.currentPlayer = null;
+
+    var grid = [ [0,1,2],[3,4,5],[6,7,8],
+                 [0,3,6],[1,4,7],[2,5,8],
+                 [0,4,8],[2,4,6] ];
+
+    this.setNextTurn = function () {
+      if (this.currentPlayer === this.player1) {
+        this.currentPlayer = this.player2;
+      }
+      else {
+        this.currentPlayer = this.player1;
+      }
+    };
+
+    this.checkForWinner = function () {
+      // Because (NaN === NaN) is always false, we can safely assume
+      // that if three spaces in a row are the same, all three spaces are
+      // marked by a player, and not all empty.
+
+      for(var i = 0; i < grid.length; i++) {
+        space1 = spaces[ grid[i][0] ];
+        space2 = spaces[ grid[i][1] ];
+        space3 = spaces[ grid[i][2] ];
+
+        if (space1 === space2 && space2 === space3) {
+
+          if (this.currentPlayer === this.player1) {
+            this.player1Score = this.player1Score + 1;
+          } else {
+            this.player2Score = this.player2Score + 1;
+          }
+
+          console.log(this.player1 + " - score - " + this.player1Score)
+          console.log(this.player2 + " - score - " + this.player2Score)
+
+          return true;
+        }
+      }
+      return false;
+    };
+
+    this.markSpace = function(spaceNum) {
+      spaces[spaceNum] = this.currentPlayer;
+    };
+
+    this.spaceBlank = function(spaceNum) {
+      return typeof( spaces[spaceNum] ) === 'number';
+    }
+
+    this.start = function() {
+      spaces = [ NaN, NaN, NaN,
+                 NaN, NaN, NaN,
+                 NaN, NaN, NaN ];
+
+      this.player1 = 'veggies';
+      this.player2 = 'junkfood';
+      this.currentPlayer = null;
+
+      this.setNextTurn();
+    }
+
+    // Start the game
+    this.start();
+  };
+
+  window.game = new Game();
+})();
---
+++ js/view.js
@@ -0,0 +1,39 @@
+(function() {
+  $('#turn').text("It's " + game.currentPlayer + "'s turn.");
+  $('#score1').text(game.player1 + ": " + game.player1Score);
+  $('#score2').text(game.player2 + ": " + game.player2Score);
+
+  $('#board').on('click', '.space', function (e) {
+    var spaceNum = $(e.currentTarget).index();
+    console.log('You clicked on space #' + spaceNum);
+
+    // Marks the space with the current player's name
+    if ( game.spaceBlank(spaceNum) ) {
+      game.markSpace(spaceNum);
+      // Adds a class to elem so css can take care of the visuals
+      $('#board .space:eq(' + spaceNum + ')').addClass(game.currentPlayer);
+
+      if ( game.checkForWinner() ) {
+        console.log(game.currentPlayer + 'won!');
+        $(document).trigger("game-win", game.currentPlayer);
+      } else {
+        game.setNextTurn();
+        $('#turn').text("It's " + game.currentPlayer + "'s turn.");
+      }
+    }
+  });
+
+  $(document).on('game-win', function (e, winner) {
+    $('#turn').text(winner + " won the game!");
+    $('#score1').text(game.player1 + ": " + game.player1Score);
+    $('#score2').text(game.player2 + ": " + game.player2Score);
+
+    // setText(winner + " won the game!");
+    if ( confirm("Do you want to play again?") ) {
+      $('#board .space').removeClass(game.player1);
+      $('#board .space').removeClass(game.player2);
+
+      game.start();
+    }
+  });
+})();
